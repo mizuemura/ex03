b2edbb910f0c7953d454a87f89b1793087615e42
Juhun Lee <rangho@yonsei.ac.kr>
2025-09-08 14:41:24 +0900

    feat: implement greet feature

--- __init__.py
+++ __init__.py
@@ -9,3 +9,7 @@ async def root():
 @app.get("/ping")
 async def ping():
     return {"message": "pong"}
+
+@app.get("/greet/{name}")
+async def greet(name: str):
+    return {"message": f"Hello, {name}!"}
